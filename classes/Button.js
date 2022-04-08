8c86c1be4216b524236e038e3e5dc21d65ed18a4
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-04-08 16:18:23 -0700

    moved classes into source

--- classes/Button.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Button {
-    constructor(nameInput) {
-        this.name = nameInput;
-    }
-
-    execute(interaction, args) { };
-}
--- classes/CommandSet.js
+++
@@ -1,9 +0,0 @@
-// Command Linking Class
-module.exports = class CommandSet {
-	constructor(nameInput, descriptionInput, cullforNonManagers, fileNamesInput) {
-		this.name = nameInput;
-		this.description = descriptionInput;
-		this.managerCommands = cullforNonManagers;
-		this.fileNames = fileNamesInput;
-	}
-}
