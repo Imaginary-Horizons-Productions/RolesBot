24fa0b4a61b60ac6c5caf029c459486a2b8a060b
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-05-15 11:58:07 -0700

    Update to Discord.js v13.7.0 Fixes #6

--- blueprints/command_blueprint.js
+++ blueprints/command_blueprint.js
@@ -1,7 +1,13 @@
 const Command = require('../classes/Command.js');
 
 const options = [
-	{ type: "", name: "", description: "", required: false, choices: {} }
+	{
+		type: "",
+		name: "",
+		description: "",
+		required: false,
+		choices: [] // elements are objects with properties: name, value
+	}
 ];
 module.exports = new Command("name", "description", false, false, options);
 
