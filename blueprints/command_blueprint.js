c66dce01bfdadddf02901708df645f6cdfb0ea37
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-05-23 21:00:24 -0700

    Add support for subcommands Fixes #11

--- blueprints/command_blueprint.js
+++ blueprints/command_blueprint.js
@@ -9,7 +9,22 @@ const options = [
 		choices: [] // elements are objects with properties: name, value
 	}
 ];
-module.exports = new Command("name", "description", false, false, options);
+const subcommands = [
+	{
+		name: "",
+		description: "",
+		optionsInput: [
+			{
+				type: "",
+				name: "",
+				description: "",
+				required: false,
+				choices: [] // elements are objects with properties: name, value
+			}
+		]
+	}
+];
+module.exports = new Command("name", "description", false, false, options, subcommands);
 
 module.exports.execute = (interaction) => {
 	// Command specifications go here
--- source/classes/Command.js
+++ source/classes/Command.js
@@ -22,6 +22,25 @@ module.exports = class Command {
 				return built;
 			})
 		})
+		subcommandsInput.forEach(subcommand => {
+			this.data.addSubcommand(built => {
+				built.setName(subcommand.name).setDescription(subcommand.description);
+				subcommand.optionsInput.forEach(option => {
+					built[`add${option.type}Option`](subBuilt => {
+						subBuilt.setName(option.name).setDescription(option.description).setRequired(option.required);
+						if (option.choices === null || option.choices === undefined) {
+							throw `Error: ${this.nameInput} (${descriptionInput}) ${option.type} Option was nullish.`;
+						}
+						let choiceEntries = Object.entries(option.choices);
+						if (choiceEntries.length) {
+							subBuilt.addChoices(Object.entries(option.choices));
+						}
+						return subBuilt;
+					})
+				})
+				return built;
+			})
+		})
 	}
 
 	execute(interaction) { }
